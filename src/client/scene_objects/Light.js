df9a25cccbe8b8a9d99ace570999123007a97774
HurrmanYang <yangxuyu.fish@gmial.com>
2013-03-26 19:00:43 +0800

    Remove some useless files.

--- src/client/scene_objects/Light.js
+++
@@ -1,7 +0,0 @@
-VENUS.Light = function(lightValueVector3){
-	VENUS.SceneObject.call(this);
-	this.lightValue = lightValueVector3;
-}
-
-VENUS.Light.prototype = Object.create(VENUS.SceneObject.prototype);
-
